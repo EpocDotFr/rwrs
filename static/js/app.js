a4384fcfd48357d4b94a836ea0ef0de5c1760b4c
EpocDotFr <contact@epoc.fr>
2017-11-07 17:35:41 +0100

    Charts

--- static/js/app.js
+++ static/js/app.js
@@ -358,7 +358,7 @@ playersChartsFeature = {
             area: false,
             y_extended_ticks: true,
             x_extended_ticks: true,
-            top: 10,
+            top: 20,
             bottom: 35,
             left: 30,
             right: 15,
