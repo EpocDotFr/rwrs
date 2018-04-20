b6e699d6732ecd6c444a1fb891bccd83adbcb40c
EpocDotFr <contact@epoc.fr>
2018-04-20 14:01:35 +0200

    Little things

--- static/js/map.js
+++ static/js/map.js
@@ -3,6 +3,7 @@
  */
 mapFeature = {
     defaultZoom: 1,
+    overlays: {},
 
     /**
      * Initialize the feature on the Map details page.
@@ -22,9 +23,6 @@ mapFeature = {
             this.map.unproject([2048, 0], this.map.getMaxZoom())
         );
 
-        this.map.setMaxBounds(this.bounds);
-        this.map.setView(this.bounds.getCenter());
-
         this.mapLayer = L.tileLayer(this.tilesUrl, {
             minZoom: this.minZoom,
             maxZoom: this.maxZoom,
@@ -33,12 +31,29 @@ mapFeature = {
             errorTileUrl: this.tileNotFoundUrl
         }).addTo(this.map);
 
-        L.control.scale({
+        // FIXME Scale is wrong
+        this.scaleControl = L.control.scale({
             position: 'bottomleft'
         }).addTo(this.map);
 
-        L.control.zoom({
+        this.zoomControl = L.control.zoom({
             position: 'bottomright'
         }).addTo(this.map);
+
+        // TODO Temporary according to this screenshot https://d1u5p3l4wpay3k.cloudfront.net/runningwithrifles_gamepedia/b/bd/Mapview.jpg?version=41b530749819057bafe70109299d99d6
+        this.overlays['<i class="fa fa-map"></i> Test'] = L.layerGroup([
+            L.marker(this.map.unproject([1024, 1024], this.map.getMaxZoom())).bindPopup('Test')
+        ]);
+
+        this.layersControl = L.control.layers(
+            null,
+            this.overlays,
+            {
+                position: 'topright'
+            }
+        ).addTo(this.map);
+
+        this.map.setMaxBounds(this.bounds);
+        this.map.setView(this.bounds.getCenter());
     }
 }
