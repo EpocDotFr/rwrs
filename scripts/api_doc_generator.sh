80e6fa7534d2fded06e128bae2691e691821e73c
Epoc <contact@epoc.fr>
2018-11-09 23:13:12 +0100

    API doc

--- scripts/api_doc_generator.sh
+++ scripts/api_doc_generator.sh
@@ -2,9 +2,15 @@
 #
 # RWRS API documentation generator
 #
-# Generate the RWRS API documentation. aglio must be installed. Input API Blueprint files are located in docs/api, output
-# file is saved in static/api_doc.html.
+# Generates the RWRS API documentation. aglio must be installed. Input API Blueprint files are located in docs/api,
+# output file is saved in static/api_doc.html.
 # Refs:
 # - https://github.com/danielgtaylor/aglio
 
 set -e # Makes any subsequent failing commands to exit the script immediately
+
+aglio \
+    -i docs/api/index.apib \
+    -o static/api_doc.html \
+    --theme-variables flatly \
+    --theme-full-width
