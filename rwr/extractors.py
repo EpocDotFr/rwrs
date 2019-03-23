ac6d333e2dbfcb611dd013b43c461ab12152d4a3
Epoc <contact@epoc.fr>
2019-03-23 22:23:54 +0100

    Change images output dir

--- rwr/extractors.py
+++ rwr/extractors.py
@@ -346,7 +346,7 @@ class ItemsExtractor(BaseExtractor):
             new_weapon_image = Image.new('RGBA', self.images_size)
             new_weapon_image.paste(weapon_image, paste_pos)
 
-            output_dir = os.path.join(app.config['ITEMS_IMAGES_DIR'], game_type)
+            output_dir = os.path.join(app.config['ITEMS_IMAGES_DIR'], game_type, weapon_slot)
 
             if not os.path.isdir(output_dir):
                 os.makedirs(output_dir)
@@ -449,7 +449,7 @@ class ItemsExtractor(BaseExtractor):
             new_throwable_image = Image.new('RGBA', self.images_size)
             new_throwable_image.paste(throwable_image, paste_pos)
 
-            output_dir = os.path.join(app.config['ITEMS_IMAGES_DIR'], game_type)
+            output_dir = os.path.join(app.config['ITEMS_IMAGES_DIR'], game_type, 'throwables')
 
             if not os.path.isdir(output_dir):
                 os.makedirs(output_dir)
