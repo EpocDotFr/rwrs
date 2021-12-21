a0a933c458522610ff749fc1f08f11762e8d4302
EpocDotFr <contact@epoc.fr>
2021-12-21 17:46:45 +0100

    Add public commands stubs

--- discord/commands.py
+++ discord/commands.py
@@ -1,5 +1,6 @@
 from flask_discord_interactions import Channel
 from rwrs import discord_interactions
+from . import constants
 
 maintenance_command_group = discord_interactions.command_group('maintenance')
 motd_command_group = discord_interactions.command_group('motd')
@@ -7,32 +8,32 @@ user_command_group = discord_interactions.command_group('user')
 event_command_group = discord_interactions.command_group('event')
 
 
-@discord_interactions.command('cc', 'Clears the RWRS cache')
+@discord_interactions.command('cc', 'Clears RWRS cache')
 def cc(ctx):
     return 'TODO'
 
 
-@discord_interactions.command('say', 'Makes the bot to say something', annotations={'channel': 'The channel in which the message should be sent', 'message': 'The message to send'})
+@discord_interactions.command('say', 'Makes the bot to say something')
 def say(ctx, channel: Channel, message: str):
     return 'TODO'
 
 
-@maintenance_command_group.command('enable', 'Enables the maintenance mode')
+@maintenance_command_group.command('enable', 'Enables maintenance mode')
 def maintenance_enable(ctx):
     return 'TODO'
 
 
-@maintenance_command_group.command('disable', 'Disables the maintenance mode')
+@maintenance_command_group.command('disable', 'Disables maintenance mode')
 def maintenance_disable(ctx):
     return 'TODO'
 
 
-@motd_command_group.command('set', 'Sets the MOTD', annotations={'message': 'Content of the MOTD. Markdown is supported'})
+@motd_command_group.command('set', 'Sets MOTD', annotations={'message': 'Markdown is supported'})
 def motd_set(ctx, message: str):
     return 'TODO'
 
 
-@motd_command_group.command('remove', 'Removes the MOTD')
+@motd_command_group.command('remove', 'Removes MOTD')
 def motd_remove(ctx):
     return 'TODO'
 
@@ -47,11 +48,61 @@ def user_api_unban(ctx, user_id: int):
     return 'TODO'
 
 
-@event_command_group.command('set', 'Sets the next RWR event', annotations={'name': 'Name of the event', 'datetime': 'Date and time when the event will begin. Format: YYYY-MM-DD HH:mm ZZZ', 'server_ip_and_port': 'The server IP and port where the event will take place. Format: {ip}:{port}'})
+@event_command_group.command('set', 'Sets next RWR event', annotations={'datetime': 'Format: YYYY-MM-DD HH:mm ZZZ', 'server_ip_and_port': 'Format: {ip}:{port}'})
 def event_set(ctx, name: str, datetime: str, server_ip_and_port: str = None):
     return 'TODO'
 
 
-@event_command_group.command('remove', 'Removes the next RWR event')
+@event_command_group.command('remove', 'Removes next RWR event')
 def event_remove(ctx):
+    return 'TODO'
+
+
+@discord_interactions.command('info', 'Displays information about the bot')
+def info(ctx):
+    return 'TODO'
+
+
+@discord_interactions.command('stats', 'Displays stats about a given player', annotations={'username': 'A full RWR username', 'database': 'Official (ranked) server type to get data from (defaults to invasion)', 'date': 'Get stats for given date (defaults to current)'})
+def stats(ctx, username: str, database: constants.Databases = constants.Databases.invasion, date: str = None):
+    return 'TODO'
+
+
+@discord_interactions.command('evolution', 'Displays evolution of specified stat', annotations={'username': 'A full RWR username', 'database': 'Official (ranked) server type to get data from (defaults to invasion)'})
+def evolution(ctx, username: str, type: constants.EvolutionTypes, database: constants.Databases = constants.Databases.invasion):
+    return 'TODO'
+
+
+@discord_interactions.command('whereis', 'Displays information about the server the given player is playing on', annotations={'username': 'A RWR username (may be partial)'})
+def whereis(ctx, username: str):
+    return 'TODO'
+
+
+@discord_interactions.command('server', 'Displays information about the given server', annotations={'username': 'A server name (may be partial)'})
+def server(ctx, name: str):
+    return 'TODO'
+
+
+@discord_interactions.command('now', 'Displays numbers about the current players and servers')
+def now(ctx):
+    return 'TODO'
+
+
+@discord_interactions.command('servers', 'Displays first {} currently active servers with room'.format(constants.SERVERS_LIMIT), annotations={'ranked_only': 'Only return ranked (official) servers'})
+def servers(ctx, type: constants.ServerTypes = None, ranked_only: bool = False):
+    return 'TODO'
+
+
+@discord_interactions.command('top', 'Displays the top {} players'.format(constants.PLAYERS_LIMIT), annotations={'database': 'Official (ranked) server type to get data from (defaults to invasion)'})
+def top(ctx, sort: constants.PlayerSorts = constants.PlayerSorts.score, database: constants.Databases = constants.Databases.invasion):
+    return 'TODO'
+
+
+@discord_interactions.command('pos', 'Highlights the given player in the leaderboard', annotations={'username': 'A full RWR username', 'database': 'Official (ranked) server type to get data from (defaults to invasion)'})
+def pos(ctx, username: str, sort: constants.PlayerSorts = constants.PlayerSorts.score, database: constants.Databases = constants.Databases.invasion):
+    return 'TODO'
+
+
+@discord_interactions.command('compare', 'Compare stats between two players', annotations={'source_username': 'A full RWR username', 'target_username': 'A full RWR username', 'database': 'Official (ranked) server type to get data from (defaults to invasion)', 'date': 'Get stats for given date (defaults to current)'})
+def compare(ctx, source_username: str, target_username: str, database: constants.Databases = constants.Databases.invasion, date: str = None):
     return 'TODO'
---
+++ discord/constants.py
@@ -0,0 +1,37 @@
+from enum import Enum
+import rwr.constants
+import helpers
+
+PLAYERS_LIMIT = 24
+SERVERS_LIMIT = 10
+
+Databases = Enum('Databases', {d: d for d in rwr.constants.VALID_DATABASES})
+
+EVOLUTION_TYPES = {
+    'ratio': {'name': 'K/D ratio', 'column': 'kd_ratio'},
+    'score': {'name': 'score', 'column': 'score'},
+    'position': {'name': 'position (by XP)', 'column': 'leaderboard_position'},
+}
+
+EvolutionTypes = Enum('EvolutionTypes', {d: d for d in EVOLUTION_TYPES.keys()})
+
+ServerTypes = Enum('ServerTypes', {d: d for d in rwr.constants.VALID_SERVER_TYPES})
+
+PLAYER_SORTS = {
+    'score': {'name': 'score', 'value': rwr.constants.PlayersSort.SCORE.value, 'getter': lambda player: player.score_display},
+    'xp': {'name': 'experience', 'value': rwr.constants.PlayersSort.XP.value, 'getter': lambda player: player.xp_display},
+    'kills': {'name': 'kills', 'value': rwr.constants.PlayersSort.KILLS.value, 'getter': lambda player: player.kills_display},
+    'deaths': {'name': 'deaths', 'value': rwr.constants.PlayersSort.DEATHS.value, 'getter': lambda player: player.deaths_display},
+    'ratio': {'name': 'K/D ratio', 'value': rwr.constants.PlayersSort.KD_RATIO.value, 'getter': lambda player: player.kd_ratio},
+    'time': {'name': 'time played', 'value': rwr.constants.PlayersSort.TIME_PLAYED.value, 'getter': lambda player: helpers.humanize_seconds_to_hours(player.time_played)},
+    'streak': {'name': 'longest kill streak', 'value': rwr.constants.PlayersSort.LONGEST_KILL_STREAK.value, 'getter': lambda player: player.longest_kill_streak_display},
+    'tk': {'name': 'teamkills', 'value': rwr.constants.PlayersSort.TEAMKILLS.value, 'getter': lambda player: player.teamkills_display},
+    'heals': {'name': 'soldiers healed', 'value': rwr.constants.PlayersSort.SOLDIERS_HEALED.value, 'getter': lambda player: player.soldiers_healed_display},
+    'shots': {'name': 'shots fired', 'value': rwr.constants.PlayersSort.SHOTS_FIRED.value, 'getter': lambda player: player.shots_fired_display},
+    'distance': {'name': 'distance moved (km)', 'value': rwr.constants.PlayersSort.DISTANCE_MOVED.value, 'getter': lambda player: player.distance_moved},
+    'throws': {'name': 'throwables thrown', 'value': rwr.constants.PlayersSort.THROWABLES_THROWN.value, 'getter': lambda player: player.throwables_thrown_display},
+    'vehicles': {'name': 'vehicles destroyed', 'value': rwr.constants.PlayersSort.VEHICLES_DESTROYED.value, 'getter': lambda player: player.vehicles_destroyed_display},
+    'targets': {'name': 'targets destroyed', 'value': rwr.constants.PlayersSort.TARGETS_DESTROYED.value, 'getter': lambda player: player.targets_destroyed_display},
+}
+
+PlayerSorts = Enum('PlayerSorts', {d: d for d in PLAYER_SORTS.keys()})
