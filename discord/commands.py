2396448babd48b351d5c0a7e1948634acdd72d60
EpocDotFr <contact@epoc.fr>
2021-12-21 20:42:52 +0100

    Implement "info" command

--- discord/commands.py
+++ discord/commands.py
@@ -136,7 +136,11 @@ def event_remove(
 def info(
     ctx
 ):
-    return 'TODO'
+    return '\n'.join([
+        ':information_source: Hi! I was created by <@{}> around the beginning of March 2018.'.format(constants.MY_DISCORD_ID),
+        'Like the RWRS website, my brain is powered by the Python programming language. More info: https://rwrstats.com/about',
+        'P.S. You look beautiful today.'
+    ])
 
 
 @discord_interactions.command(
