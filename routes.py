7b783f132db06644709df16ab9c8e89696787aee
EpocDotFr <contact@epoc.fr>
2022-03-27 21:18:15 +0200

    Add delete RWR account link

--- routes.py
+++ routes.py
@@ -185,6 +185,27 @@ def remove_friend(username):
     return redirect(helpers.get_next_url())
 
 
+@app.route('/my-rwr-accounts/delete/<int:rwr_account_id>')
+@login_required
+def delete_rwr_account(rwr_account_id):
+    rwr_account = RwrAccount.query.get(rwr_account_id)
+
+    if not rwr_account or rwr_account.user_id != current_user.id:
+        abort(404)
+
+    # if True:
+    #     flash('Friend removed. Sad.', 'success')
+    # else:
+    #     flash('Friend not found.', 'error')
+
+    # return redirect(helpers.get_next_url())
+
+    return render_template(
+        'users/delete_rwr_account.html',
+        rwr_account=rwr_account
+    )
+
+
 @app.route('/about')
 def about():
     return render_template(
