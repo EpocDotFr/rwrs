332e233b07a0c24d18df583ac38be6b7cb1f130f
EpocDotFr <contact@epoc.fr>
2022-04-29 14:31:55 +0200

    Add new pending_delete column

--- models.py
+++ models.py
@@ -623,6 +623,7 @@ class RwrAccount(db.Model):
 
     type = db.Column(db.Enum(RwrAccountType), nullable=False)
     username = db.Column(db.String(16), nullable=False)
+    pending_delete = db.Column(db.Boolean, nullable=False, default=False)
     created_at = db.Column(ArrowType, default=lambda: arrow.utcnow().floor('minute'), nullable=False)
     updated_at = db.Column(ArrowType, default=lambda: arrow.utcnow().floor('minute'), onupdate=lambda: arrow.utcnow().floor('minute'), nullable=False)
 
